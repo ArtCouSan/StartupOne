abf0ee3fc9f27d9b527362c356d711af6c13b4bc
artcousan <artcousan@gmail.com>
2020-06-09 22:17:10 -0300

    Primeiras configs

---
+++ changeTheWold/eureka-client/src/main/java/br/com/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,17 @@
+package br.com.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
