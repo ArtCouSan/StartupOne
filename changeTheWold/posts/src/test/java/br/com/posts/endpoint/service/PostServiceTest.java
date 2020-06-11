8f5554771ae38ded8443bc00101399c8fef7ea9e
artcousan <artcousan@gmail.com>
2020-06-11 14:18:58 -0300

    feat: test save post

--- changeTheWold/posts/src/test/java/br/com/posts/endpoint/service/PostServiceTest.java
+++ changeTheWold/posts/src/test/java/br/com/posts/endpoint/service/PostServiceTest.java
@@ -1,36 +1,55 @@
 package br.com.posts.endpoint.service;
 
+import br.com.posts.endpoint.dto.PostSaveDTO;
 import br.com.posts.endpoint.entity.Post;
 import br.com.posts.endpoint.enums.PostStatusEnum;
+import br.com.posts.endpoint.repository.PostRepository;
+import br.com.posts.endpoint.service.impl.PostServiceImpl;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.validation.constraints.AssertTrue;
+
+import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class PostServiceTest {
 
-    @Autowired
-    PostService postService;
+    @Mock
+    private PostRepository postRepository;
+
+    @InjectMocks
+    private PostServiceImpl postService;
 
     @Test
     public void savePostTest() {
-
-
-
+        PostSaveDTO postSaveDTO = createSavePostDTO();
+        Post post = createPostDTO();
+        when(this.postRepository.save(post)).thenReturn(post);
+        post = this.postService.savePost(postSaveDTO);
+        Assert.assertNotNull(post);
     }
 
-    private Post createPost() {
+    private Post createPostDTO() {
         Post post = new Post();
         post.setId(1l);
         post.setStatus(PostStatusEnum.ACTIVE);
         post.setTitle("Test title");
         post.setDescription("Test Description");
         post.setDateInsert(LocalDateTime.now());
+        return post;
+    }
+
+    private PostSaveDTO createSavePostDTO() {
+        PostSaveDTO post = new PostSaveDTO();
+        post.setTitle("Test title");
+        post.setDescription("Test Description");
         return post;
     }
 
